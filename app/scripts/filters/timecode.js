cfbc4167d389f6578bb24163d65817d12c872ddb
Alexashooo <aleksandar.sunjic@gmail.com>
2016-11-12 20:03:44 +0100

    Assignment 11 Filters is finished

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,24 +1,16 @@
 (function() {
      function timecode() {
          return function(seconds) {
-             var seconds = Number.parseFloat(seconds);
+             var seconds = seconds;
              
-             if (Number.isNaN(seconds)) {
-                    return '-:--';
-             }
+             if(seconds === "NaN"){
+               return "--:--"  
+             } else {
              
-             var wholeSeconds = Math.floor(seconds);
-             var minutes = Math.floor(wholeSeconds / 60);
-             var remainingSeconds = wholeSeconds % 60;
- 
-             var output = minutes + ':';
- 
-             if (remainingSeconds < 10) {
-                 output += '0';   
-             }
- 
-             output += remainingSeconds;
+             var output = buzz.toTimer(seconds);
+                 
              return output;
+             }
          };
      }
  
